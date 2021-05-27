89b247cc5dcdc8463eb20012e4723a5b009333cd
Jaime Reina Durán <jaimereinaduran@gmail.com>
2021-05-27 22:25:13 +0200

    Cambio en la coneccion de la base de datos

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://JaimeRedu:usuariojaimeredu@a-la-mesa.0ra72.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://JaimeRedu:usuariojaimeredu@a-la-mesa.0ra72.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false
