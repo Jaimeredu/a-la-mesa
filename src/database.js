2d1c2bdab0b68bf4597cf5f5bde155e5ee1e8e36
Jaime Reina Durán <jaimereinaduran@gmail.com>
2021-05-27 22:20:09 +0200

    segundo cambio

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/a-la-mesa', {
+mongoose.connect('mongodb+srv://JaimeRedu:usuariojaimeredu@a-la-mesa.0ra72.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false
